5b513a8fcbce3860648cffa2ed34cada43bdb224
Rafael Meha <97791234+RafaelMeha@users.noreply.github.com>
2023-12-05 10:45:08 +0000

    Co-authored-by: Diogo Santos <diogo-s4ntos@users.noreply.github.com>

--- public/JS/contact.js
+++ public/JS/contact.js
@@ -7,3 +7,22 @@ function validarLetras(input){
     //Remove qualquer caractere que não seja uma letra (maiúscula ou minúscula)
     input.value = input.value.replace(/[^A-Za-z]/g, '');
 }
+
+document.getElementById('send').addEventListener('click', () => {
+    var form = document.getElementById("myForm");
+    var elements = form.elements;
+    var hasEmptyField = false;
+
+    for (var i = 0; i < elements.length; i++) {
+        if (elements[i].value === '') {
+            elements[i].classList.add('error');
+            hasEmptyField = true;
+        }else{
+            elements[i].classList.add('filled');
+        }
+    }
+
+    // if (hasEmptyField) {
+    //     alert("Please enter all mandatory data!");
+    // }
+});
