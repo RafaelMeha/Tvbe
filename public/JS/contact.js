0dd1955cfee945e242c60495aeb3a9d4bf883dc9
Rafael Meha <97791234+RafaelMeha@users.noreply.github.com>
2023-12-05 13:54:43 +0000

    Co-authored-by: Diogo Santos <diogo-s4ntos@users.noreply.github.com>

--- public/JS/contact.js
+++ public/JS/contact.js
@@ -26,3 +26,27 @@ document.getElementById('send').addEventListener('click', () => {
     //     alert("Please enter all mandatory data!");
     // }
 });
+
+function loadCountries() {
+    var select = document.getElementById("countries");
+
+    // Utilizando a API RestCountries para obter a lista de países
+    fetch("https://restcountries.com/v3.1/all")
+        .then(response => response.json())
+        .then(data => {
+            // Ordena os países em ordem alfabética
+            data.sort((a, b) => a.name.common.localeCompare(b.name.common));
+
+            // Adiciona os países à lista de seleção
+            data.forEach(country => {
+                var option = document.createElement("option");
+                // option.value = country.cca2;
+                option.text = country.name.common;
+                select.appendChild(option);
+            });
+        })
+        .catch(error => console.error("Erro ao carregar países:", error));
+}
+
+// Chama a função para carregar os países quando a página carregar
+window.onload = loadCountries;
