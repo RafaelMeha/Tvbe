a721061d9b6122eb539c66dc288c68e2ed15ffc4
Rafael Meha <97791234+RafaelMeha@users.noreply.github.com>
2024-01-19 10:21:39 +0000

    sublogin.html

---
+++ public/JS/subLogin.js
@@ -0,0 +1,16 @@
+// Add an event listener to the subscription buttons
+const standardButton = document.getElementById('standard-button');
+const duoButton = document.getElementById('duo-button');
+const familyButton = document.getElementById('family-button');
+
+standardButton.addEventListener('click', function () {
+    this.textContent = '✓ Standard Premium';
+});
+
+duoButton.addEventListener('click', function () {
+    this.textContent = '✓ Duo premium';
+});
+
+familyButton.addEventListener('click', function () {
+    this.textContent = '✓ Family premium';
+});
